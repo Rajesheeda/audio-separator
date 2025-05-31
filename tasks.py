0d2f41f6eedc2494110791550675873f28827ac2
EEDA RAJESH <30622739+Rajesheeda@users.noreply.github.com>
2025-05-31 16:22:41 +0530

    Update tasks.py added env

--- tasks.py
+++ tasks.py
@@ -1,7 +1,12 @@
 import os
+from dotenv import load_dotenv
 from celery import Celery
 
-redis_url = os.getenv("REDIS_URL", "rediss://default:AaxkAAIjcDFlOTg0N2NjYWEzYWY0NjFlOGM5NWFmZTBhY2ZkZjg3NHAxMA@smiling-dolphin-44132.upstash.io:6379")
+# Load variables from .env file
+load_dotenv()
+
+# Get Redis URL from environment
+redis_url = os.getenv("REDIS_URL")
 
 app = Celery('tasks', broker=redis_url)
 
@@ -13,9 +18,11 @@ def separate_audio_task(file_path):
     separator = Separator('spleeter:2stems')
     separator.separate_to_file(file_path, 'outputs')
 
-    base_path = f'outputs/{os.path.splitext(os.path.basename(file_path))[0]}'
-    vocals_path = f'{base_path}/vocals.wav'
-    instrumental_path = f'{base_path}/accompaniment.wav'
-    
-    return vocals_path, instrumental_path
+    base = os.path.splitext(os.path.basename(file_path))[0]
+    base_path = f'outputs/{base}'
+    vocals = f'{base_path}/vocals.wav'
+    instrumental = f'{base_path}/accompaniment.wav'
+
+    return {'vocals': vocals, 'instrumental': instrumental}
+
 
